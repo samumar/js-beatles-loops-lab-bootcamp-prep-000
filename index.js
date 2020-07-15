9365a2a0afce7e9c6f16133066fae5fabe9dfe99
samumar43 <samumar43@gmail.com>
2020-07-15 14:28:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function theBeatlesPlay(musicians, instruments) {
   return list
 }
 
-const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+const fact = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(facts) {
   facts = [];
   let i = 0;
-  while (i < facts.length) {
-    facts.push(`${facts[i]} !!!`);
+  while (i < fact.length) {
+    facts.push(`${fact[i]} !!!`);
     return facts
   }
 }
