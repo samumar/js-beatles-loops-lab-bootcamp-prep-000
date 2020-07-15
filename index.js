e16b18593660c03600e62516dbad0b0a2ce0b9e4
samumar43 <samumar43@gmail.com>
2020-07-15 14:23:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ function theBeatlesPlay(musicians, instruments) {
 const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 
 function johnLennonFacts(list) {
+  facts = [];
   let i = 0;
   while (i < facts.length) {
-    list.push(`${facts[i]} !!!`);
+    facts.push(`${facts[i]} !!!`);
     return list
   }
 }
